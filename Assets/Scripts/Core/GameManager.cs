58d08139550120345eb2b6115f212a59cdc763bf
jiwooya0123 <jiwooya0123@gmail.com>
2026-07-30 17:50:43 +0900

    [Fix]: 로비 기능 구현

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -240,5 +240,4 @@ public class GameManager : MonoBehaviour
         SceneLoader.Instance.LoadScene("MainMenu"); // 시작화면 → 이후 튜토리얼(스킵 가능)
     }
 
-    public bool IsPendingEndingTransition() => pendingEndingTransition;
 }
