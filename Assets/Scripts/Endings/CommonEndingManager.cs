863ee2ded4c4d20084ba1b24475e89022972f4c2
ssoyeon0801 <ssoyeon0801@gmail.com>
2026-09-20 22:02:44 +0900

    엔딩진입 조상신 수정

--- Assets/Scripts/Endings/CommonEndingManager.cs
+++ Assets/Scripts/Endings/CommonEndingManager.cs
@@ -8,6 +8,7 @@ public class CommonEndingManager : MonoBehaviour
     [Header("Dialogue UI (조상신 대화)")]
     public GameObject dialoguePanel;
     public TMP_Text dialogueText;
+    public GameObject dialogueUI;
 
     [Header("Ending UI")]
     public TMP_Text titleText;
@@ -37,9 +38,9 @@ public class CommonEndingManager : MonoBehaviour
     {
         "그래... 처음 보는구나.",
         "내가 바로 네 조상이다.",
-        "내가 널 참 오랫동안 지켜보고 있었지... 갓난아기일 때부터 회사에 치이는 지금까지...",
-        "얼마나 고생이 많았느냐. 난 널 도와주러 온 사람이야.",
-        "그럼 어디, 지난 시간동안 얼마나 공덕을 쌓아왔는지 볼까."
+        "내가 널 참 오랫동안 지켜보고 있었지..\n갓난아기일 때부터 회사에 치이는 지금까지...",
+        "얼마나 고생이 많았느냐. \n난 널 도와주러 온 사람이야.",
+        "그럼 어디, 지난 시간동안 \n얼마나 공덕을 쌓아왔는지 볼까."
     };
     private int dialogueIndex = 0;
     private bool isDialogueEnding = false;
@@ -67,6 +68,9 @@ public class CommonEndingManager : MonoBehaviour
 
         if (ptText != null)
             ptText.gameObject.SetActive(false);
+
+        if (dialogueUI != null)
+            dialogueUI.SetActive(false);
 
         if (signboard != null)
         {
@@ -108,6 +112,10 @@ public class CommonEndingManager : MonoBehaviour
 
             dialogueText.text = currentSentence;
             isTyping = false;
+
+            if (dialogueUI != null)
+                dialogueUI.SetActive(true);
+
             return;
         }
 
@@ -120,6 +128,8 @@ public class CommonEndingManager : MonoBehaviour
         else
         {
             isDialogueEnding = true;
+
+            if (dialogueUI != null) dialogueUI.SetActive(false);
             if (dialoguePanel != null) dialoguePanel.SetActive(false);
 
             if (signboard != null)
@@ -131,6 +141,9 @@ public class CommonEndingManager : MonoBehaviour
 
     private void ShowNextDialogue()
     {
+        if (dialogueUI != null)
+            dialogueUI.SetActive(false);
+
         if (dialogueText != null)
         {
             currentSentence = ancestorDialogues[dialogueIndex];
@@ -154,6 +167,9 @@ public class CommonEndingManager : MonoBehaviour
         }
 
         isTyping = false;
+
+        if (dialogueUI != null)
+            dialogueUI.SetActive(true);
     }
 
     private IEnumerator MoveSignboard()
